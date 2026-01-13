dcaec6b7f304add511f370ac95cbcc9312a7cc37
Its-Danl <itsdanl.thai@gmail.com>
2026-01-13 12:02:23 -0500

    FullColliders
    - Made all colliders
    - Simple character movement

--- Assets/Scripts/Player/Player Controller.cs
+++ Assets/Scripts/Player/Player Controller.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody2D rb;
+
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // input handling
+        float horizontalInput = Input.GetAxis("Horizontal");
+        bool jumpInput = Input.GetButtonDown("Jump");
+
+        // movement
+        Vector2 velocity = rb.linearVelocity;
+        velocity.x = horizontalInput * moveSpeed;
+        rb.linearVelocity = velocity;
+
+        if (jumpInput)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
