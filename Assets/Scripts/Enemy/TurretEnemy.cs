e23010e6f1cff6cf7a17099c59994d261ecfe500
Its-Danl <itsdanl.thai@gmail.com>
2026-02-28 23:12:42 -0500

    turret fix
    turret faces and shoots ouch and styuff

--- Assets/Scripts/Enemy/TurretEnemy.cs
+++ Assets/Scripts/Enemy/TurretEnemy.cs
@@ -1,27 +1,46 @@
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 
 public class TurretEnemy : BaseEnemy
 {
     [SerializeField] private float fireRate = 2f;
+    [SerializeField] private Transform target;
     private float timeSinceLastFire = 0f;
+    public float range = 1f;
 
     public override void Start()
     {
-      base.Start();
-      
+        base.Start();
+        GameManager.Instance.OnPlayerSpawned += OnPlayerSpawnedCallback;
+
         if (fireRate <= 0f )
         {
             fireRate = 2f;
             Debug.LogWarning("Fire rate must be greater than 0, defaulting.");
         }
     }
+    private void OnPlayerSpawnedCallback(PlayerController player)
+    {
+        target = player.transform;
+    }
 
     private void Update()
     {
         AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
 
-        if (stateInfo.IsName("Idle"))
+        float distanceToPlayer = Vector2.Distance(transform.position, target.position);
+
+        if (target.position.x < transform.position.x)
+        {
+            sr.flipX = true;
+        }
+        else
+        {
+            sr.flipX = false;
+        }
+
+        if (stateInfo.IsName("Idle") && distanceToPlayer <= range)
         {
             if (Time.time >= timeSinceLastFire + fireRate)
             {
@@ -30,5 +49,4 @@ public class TurretEnemy : BaseEnemy
             }
         }
     }
-
 }
